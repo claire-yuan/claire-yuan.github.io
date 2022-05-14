5939dbfe47099960b8a381dd402d4861755c75e4
Claire Yuan <claireyuan6@gmail.com>
2022-05-13 22:54:07 -0700

    update layout, image transform, flex usage

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 ScrollReveal().reveal('.reveal');
-ScrollReveal().reveal('.reveal-2', { delay: 300 });
-ScrollReveal().reveal('.reveal-3', { delay: 600 });
+ScrollReveal().reveal('.reveal-2', { delay: 400 });
+ScrollReveal().reveal('.reveal-3', { delay: 800 });
