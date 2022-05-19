ce7bfeb89dc31a021e9ea31d1858cce8f86ecbcf
Claire Yuan <claireyuan6@gmail.com>
2022-05-18 22:30:01 -0700

    add breakpoints, responsive nav, meta tag

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 ScrollReveal().reveal('.reveal');
 ScrollReveal().reveal('.reveal-2', { delay: 400 });
 ScrollReveal().reveal('.reveal-3', { delay: 800 });
+
+document.getElementById("navbar-btn").onclick = function(){toggleNav()};
+
+function toggleNav() {
+    var nav = document.getElementById("nav");
+    var navStyle = window.getComputedStyle(nav);
+    if (navStyle.display == 'none') {
+        nav.style.display = "flex";
+    }
+    else if (navStyle.display === 'flex') {
+        nav.style.display = 'none';
+    }
+}
